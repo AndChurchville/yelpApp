9954f4b7d6c1d74065e7bcbec662b80b80305f49
AndChurchville <48935849+AndChurchville@users.noreply.github.com>
2019-12-12 11:31:48 -0600

    Updates

---
+++ precache-manifest.740bddd51cd087fbcf1175f6f339c845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330d62ba83f50bf2ba382e81abdb0c25",
+    "url": "/yelpApp/index.html"
+  },
+  {
+    "revision": "f46dc0081ba5119a0468",
+    "url": "/yelpApp/static/css/main.0f0c0221.chunk.css"
+  },
+  {
+    "revision": "31d5bddc2d8a48130fae",
+    "url": "/yelpApp/static/js/2.c70a0d8f.chunk.js"
+  },
+  {
+    "revision": "f46dc0081ba5119a0468",
+    "url": "/yelpApp/static/js/main.357c2fbb.chunk.js"
+  },
+  {
+    "revision": "78d0a2f72e4c61afc626",
+    "url": "/yelpApp/static/js/runtime-main.6a67e437.js"
+  }
+]);
